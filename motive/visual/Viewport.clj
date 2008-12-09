9a58346901c85f1b954fc5118cda77b11e6a91b9
Matt Revelle <matt@mars.local>
2008-12-08 20:49:09 -0500

    Changes to viewport using :super-methods.

--- motive/visual/Viewport.clj
+++ motive/visual/Viewport.clj
@@ -1,18 +1,25 @@
 (ns motive.visual.Viewport
-  (:import (com.jme.app SimpleGame)
+  (:import (com.jme.app BaseSimpleGame)
            (com.jme.bounding BoundingBox
                              BoundingSphere)
            (com.jme.math Quaternion Vector3f)
            (com.jme.scene.shape Box Sphere))
-  (:gen-class 
-   :extends com.jme.app.SimpleGame
+  (:gen-class
+   :extends com.jme.app.BaseSimpleGame
    :init ctor
    :constructors {[] []}
    :exposes {rootNode {:get getRootNode}
-             display {:get getDisplay}}))
+             display {:get getDisplay}
+             tpf {:get getTpf}
+             pause {:get getPause}}
+   :super-methods {super-update [update [float] void]
+                   super-render [render [float] void]}
+   :methods [[add [com.jme.scene.Geometry clojure.lang.IFn] void]]
+   :state state))
 
+;; The objs map contains renderable objects mapped to their update function
 (defn -ctor []
-  [[]])
+  [[] (atom {:objs {}})])
 
 (defn -simpleInitGame
   [this]
@@ -26,3 +33,31 @@
   (let [display (.getDisplay this)]
     (when display
       (.close display))))
+
+(defn -add 
+  [this obj update-fn]
+  (swap! (.state this) 
+         (fn [s]
+           (let [objs (:objs s)]
+             (assoc objs obj update-fn))))
+  (.attachChild (.getRootNode this) obj))
+
+(defn -update 
+  [this #^Float/TYPE interp]
+  (. this super-update interp)
+  
+  (when (not (.getPause this))
+    (.simpleUpdate this)
+    (.updateGeometricState (.getRootNode this) (.getTpf this) true)))
+
+(defn -render
+  [this #^Float/TYPE interp]
+  (. this super-render interp)
+
+  (let [r (.getRenderer (.getDisplay this))]
+    (.draw r (.getRootNode this))
+    (.simpleRender this)))
+
+(defn -main []
+  (let [v (motive.visual.Viewport.)]
+    (.start v)))
